e01a792bbb79da7b0d13eb7e6f7d27fd0597932a
Jouan <77906989+anniechen9025@users.noreply.github.com>
2021-04-09 00:48:14 -0700

    engineer test basic

---
+++ _tests_/Engineer.test.js
@@ -0,0 +1,15 @@
+const Engineer = require("../Engineer");
+
+describe ("Engineer", ()=>{
+    describe ("Github",() => {
+        it("");
+    });
+
+    describe("getGithub", () => {
+        it("");
+    });
+
+    describe("getRole", () => {
+        it("");
+    });
+});
