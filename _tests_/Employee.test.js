eab646559a1383f60258ed9bbc212c651b8aa6e7
Jouan <77906989+anniechen9025@users.noreply.github.com>
2021-04-09 00:43:27 -0700

    employee test basic

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,23 @@
+const Employee = require("../Employee");
+
+describe ("Employee", ()=>{
+    describe ("Initialization",() => {
+        it("");
+    });
+
+    describe("getName", () => {
+        it("");
+    });
+
+    describe("getId", () => {
+        it("");
+    });
+
+    describe("getEmail", () => {
+        it("");
+    });
+
+    describe("getRole", () => {
+        it("");
+    });
+});
