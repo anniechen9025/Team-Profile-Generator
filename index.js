1b2a1c0e5f7fb2ca0658a7bd4575c0bab0f3aa1c
Jouan <77906989+anniechen9025@users.noreply.github.com>
2021-04-10 12:48:32 -0700

    finish prmt

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const manager = require("./lib/Question")
+const manager = require("./lib/Question-manager");
+const engineer = require("./lib/Question-engineer");
+const intern = require("./lib/Question-intern");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
 
 // TODO: Create a function to write README file
 function writeToFile(context) {
@@ -18,10 +20,64 @@ function writeToFile(context) {
 };
 
 // TODO: Create a function to initialize app
-function init() {
+function prtManager() {
     inquirer.prompt(manager).then(data => {
         console.log(data);
+        
+        switch(data.choice){
+            case "Engineer":
+                prtEngineer();
+                break;
+            case "Intern":
+                prtIntern();
+                break;
+            case "done":
+                writeToFile();
+                break;
+            default:
+                return;
+        }
+    });
+};
+
+function prtEngineer() {
+    inquirer.prompt(engineer).then(data => {
+        console.log(data);
+
+        switch(data.choice){
+            case "Engineer":
+                prtEngineer();
+                break;
+            case "Intern":
+                prtIntern();
+                break;
+            case "done":
+                writeToFile();
+                break;
+            default:
+                return;
+        }
+    });
+};
+
+function prtIntern() {
+    inquirer.prompt(intern).then(data => {
+        console.log(data);
+
+        switch(data.choice){
+            case "Engineer":
+                prtEngineer();
+                break;
+            case "Intern":
+                prtIntern();
+                break;
+            case "done":
+                writeToFile();
+                break;
+            default:
+                return;
+        }
     });
 };
 // Function call to initialize app
-init();
+prtManager();
