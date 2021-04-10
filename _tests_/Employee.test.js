65cff3332da1d21c124cff4ea63d2c3116da9a9f
Jouan <77906989+anniechen9025@users.noreply.github.com>
2021-04-09 22:48:04 -0700

    employee test + js

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,23 +1,82 @@
-const Employee = require("../Employee");
+const Employee = require("../lib/Employee");
 
 describe ("Employee", ()=>{
     describe ("Initialization",() => {
-        it("");
+        it("should return to an object containinga 'number' property whe called with the 'new' keyword", () => {
+            const obj = new Employee();
+
+            expect("string" in obj).toEqual(true);
+        });
+
+        it("should set 'string' when created",() => {
+            const str = "jouan";
+
+            const obj = new Employee(str);
+
+            except(obj.name).toEqual(str);
+        });
+
+        it("should create an object with a name, an id and an email address if provided valid arguments", () => {
+            const employee = new Employee("Sarah", "01", "anniechen9025@gmail.com");
+      
+            expect(employee.name).toEqual("Sarah");
+            expect(employee.id).toEqual("01");
+            expect(employee.email).toEqual("anniechen9025@gmail.com");
+        });
+
+        it("should throw an error if provided no arguments", () => {
+   
+            const em = () => new Employee();
+      
+            expect(em).toThrow();
+        });
+
+        it("should throw an error if not provided a name, id and email", () => {
+            const em1 = () => new Employee("Sarah", "", "anniechen9025@gmail.com");
+            const em2 = () => new Employee("", "012", "anniechen9025@gmail.com");
+            const em3 = () => new Employee("Sarah", "012", "")
+      
+            const err1 = new Error("Expected parameter 'name' to be input");
+            const err2 = new Error("Expected parameter 'id' to be input");
+            const err3 = new Error("Expected parameter 'email' to be input");
+      
+            expect(em1).toThrowError(err1);
+            expect(em2).toThrowError(err2);
+            expect(em3).toThrowError(err3);
+        });
     });
 
     describe("getName", () => {
-        it("");
+        it("should return the 'Employee' object's name", () => {
+            const name = "jouan";
+
+            const obj = new Employee(name, "id", "email");const result = obj.value();
+
+            expect (result).toEqual(name);
+        });
     });
 
     describe("getId", () => {
-        it("");
+        it("should return the 'Employee' object's ID", () => {
+            const id = "1";
+            
+            const obj = new Employee("name", id, "email");const result = obj.value();
+
+            expect (result).toEqual(id);
+        });
     });
 
     describe("getEmail", () => {
-        it("");
+        it("should return the 'Employee' object's email", () => {
+            const email = "anniechen9025@gmail.com";
+
+            const obj = new Employee("name", "id", email);const result = obj.value();
+
+            expect (result).toEqual(email);
+        });
     });
 
-    describe("getRole", () => {
-        it("");
-    });
+    // describe("getRole", () => {
+    //     it("");
+    // });
 });
