a39e02a6c500584f7f14c840f75a4a33e759c4d8
Jouan <77906989+anniechen9025@users.noreply.github.com>
2021-04-09 23:06:17 -0700

    engineer test + js

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -2,14 +2,27 @@ const Engineer = require("../lib/Engineer");
 
 describe ("Engineer", ()=>{
     describe ("Github",() => {
-        it("");
+        it("should create an object with a name, an id and an email address if provided valid arguments", () => {
+            const git = "anniechen"
+
+            const employee = new Employee("Sarah", "01", "anniechen9025@gmail.com",git);
+      
+            expect(employee.github).toEqual(git)
+        });
     });
 
     describe("getGithub", () => {
-        it("");
+        it("should return the 'Engineer' object's github", () => {
+            const git = "anniechen9025";
+
+            const obj = new Employee("name", "id", "email", git);
+            const result = obj.value();
+
+            expect (result).toEqual(git);
+        });
     });
 
-    describe("getRole", () => {
-        it("");
-    });
+    // describe("getRole", () => {
+    //     it("");
+    // });
 });
