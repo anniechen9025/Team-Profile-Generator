4f7f2df4dcc82e31bef2193d1de2720836a1c865
Jouan <77906989+anniechen9025@users.noreply.github.com>
2021-04-11 23:11:48 -0700

    changing a lot and html functions

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const engineer = require("./lib/Question/Question-engineer");
 const intern = require("./lib/Question/Question-intern");
 
 // TODO: Create an array of questions for user input
-const dataArray = [];
+let dataArray = [];
+let managerArray = [];
+let engineerArray = [];
+let internArray = [];
+let htmlcards = "";
+
 
 // TODO: Create a function to write README file
 function writeToFile(context) {
@@ -19,15 +24,13 @@ function writeToFile(context) {
     });
 };
 
-// TODO: Create a function to change array into HTML format
-
 // TODO: Create a function to initialize app
 function prtManager() {
     inquirer.prompt(manager).then(data => {
-        console.log(data);
+        // console.log(data);
         dataArray.push(data);
 
-        switch(data.choice){
+        switch (data.choice) {
             case "Engineer":
                 prtEngineer();
                 break;
@@ -37,6 +40,7 @@ function prtManager() {
             case "done":
                 // writeToFile();
                 console.log(dataArray);
+                filterHtmlMaking();
                 break;
             default:
                 return;
@@ -47,10 +51,10 @@ function prtManager() {
 
 function prtEngineer() {
     inquirer.prompt(engineer).then(data => {
-        console.log(data);
+        // console.log(data);
         dataArray.push(data);
 
-        switch(data.choice){
+        switch (data.choice) {
             case "Engineer":
                 prtEngineer();
                 break;
@@ -60,6 +64,7 @@ function prtEngineer() {
             case "done":
                 // writeToFile();
                 console.log(dataArray);
+                filterHtmlMaking();
                 break;
             default:
                 return;
@@ -69,10 +74,10 @@ function prtEngineer() {
 
 function prtIntern() {
     inquirer.prompt(intern).then(data => {
-        console.log(data);
+        // console.log(data);
         dataArray.push(data);
 
-        switch(data.choice){
+        switch (data.choice) {
             case "Engineer":
                 prtEngineer();
                 break;
@@ -80,8 +85,8 @@ function prtIntern() {
                 prtIntern();
                 break;
             case "done":
-                // writeToFile();
                 console.log(dataArray);
+                filterHtmlMaking();
                 break;
             default:
                 return;
@@ -91,3 +96,119 @@ function prtIntern() {
 
 // Function call to initialize app
 prtManager();
+
+// TODO: Create a function to change array into HTML format
+function filterHtmlMaking() {
+    managerArray = dataArray.filter(function (item) {
+        return item.position === "Manager";
+    });
+    engineerArray = dataArray.filter(function (item) {
+        return item.position === "Engineer";
+    });
+    internArray = dataArray.filter(function (item) {
+        return item.position === "Intern";
+    });
+    // console.log(managerArray);
+
+    let mangerhtml = managerArray.map(function (item) {
+        return generateMhtml(item);
+    });
+    // console.log(mangerhtml);
+    let engineerhtml = engineerArray.map(function (item) {
+        return generateEhtml(item);
+    })
+    let internhtml = internArray.map(function (item) {
+        return generateIhtml(item);
+    })
+
+    const htmlArray = mangerhtml.concat(engineerhtml, internhtml);
+    // console.log(htmlArray);
+    htmlcards = htmlArray.join("");
+    const html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Project Team</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+</head>
+<body>
+    <div class = "container">
+        <div class = "row">
+            <div class = "col-12 mb-3">
+                <h1 class = "text-center">My Project Team</h1>
+            </div>
+        </div>
+    </div>
+    <div class = "container">
+        <div class = "row">
+            <div class = "col-12 d-flex justify-content-center">
+                ${htmlcards}
+            </div>
+        </div>
+    </div>
+</body>
+</html>`;
+    writeToFile(html);
+};
+
+
+let generateMhtml = (managerArray) => {
+    return `   
+    <div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card title">${managerArray.name}</h2>
+        <h3 class="card-title">${managerArray.position}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${managerArray.id}</li>
+            <li class="list-group-item">Email: ${managerArray.email}</li>
+            <li class="list-group-item">Office Number: ${managerArray.office}</li>
+        </ul>
+    </div>
+</div>`
+};
+let generateEhtml = (engineerArray) => {
+    return `   
+    <div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card title">${engineerArray.name}</h2>
+        <h3 class="card-title">${engineerArray.position}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${engineerArray.id}</li>
+            <li class="list-group-item">Email: ${engineerArray.email}</li>
+            <li class="list-group-item">Github: ${engineerArray.github}</li>
+        </ul>
+    </div>
+</div>`
+};
+let generateIhtml = (internArray) => {
+    return `   
+    <div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card title">${internArray.name}</h2>
+        <h3 class="card-title">${internArray.position}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${internArray.id}</li>
+            <li class="list-group-item">Email: ${internArray.email}</li>
+            <li class="list-group-item">School: ${internArray.school}</li>
+        </ul>
+    </div>
+</div>`
+};
+
+
+
+// let managerArray = [];
+// for(let i=0; i < dataArray.length; i++){
+//     if (dataArray.position === "Manager"){
+//         managerArray.push(dataArray[i]);
+//     }
+// };
+
